9a1b45fb8879b79287f523579ff263cca060179a
mehdi <mehdiadq@gmail.com>
2021-12-18 01:32:56 +0330

    refactored, web improved

--- cmd/web/web.go
+++ cmd/web/web.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -19,6 +22,52 @@ func main() {
 	fs := http.FileServer(http.Dir("../../ui/static/"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
+	http.HandleFunc("/", home)
+	http.HandleFunc("/upload", upload)
+	http.HandleFunc("/panel", panel)
+	http.HandleFunc("/download", download)
+	http.HandleFunc("/view", view)
+
+	http.ListenAndServe(":6050", nil)
+}
+
+func upload(w http.ResponseWriter, r *http.Request) {
+	// max size: 10MB
+	r.ParseMultipartForm(10 << 20)
+
+	file, handler, err := r.FormFile("myFile")
+	if err != nil {
+		fmt.Println("failed to retrieve the file")
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	// save the file
+	dst, err := os.Create("../../input/uploaded/" + handler.Filename)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer dst.Close()
+
+	if _, err := io.Copy(dst, file); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	http.Redirect(w, r, "/panel", http.StatusTemporaryRedirect)
+}
+
+func panel(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.New("panel.html").ParseFiles("../../ui/template/panel.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	tmpl.Execute(w, nil)
+}
+
+func download(w http.ResponseWriter, r *http.Request) {
 	src, err := os.ReadFile("../../input/source.c")
 	if err != nil {
 		log.Fatal(err)
@@ -34,19 +83,53 @@ func main() {
 		tokens = append(tokens, tok)
 	}
 
-	tmpl, err := template.New("index.html").ParseFiles("../../ui/template/index.html")
+	jsonData, err := json.MarshalIndent(&tokens, "", "	")
+	if err != nil {
+		log.Println("json marshalling failed")
+	}
+
+	jsonFile, err := os.Create("../../ui/static/json/tokens.json")
+	if err != nil {
+		log.Println("creating json file failed")
+	}
+
+	jsonFile.Write(jsonData)
+
+	http.Redirect(w, r, "/static/json/tokens.json", 303)
+}
+
+func view(w http.ResponseWriter, r *http.Request) {
+	src, err := os.ReadFile("../../input/source.c")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		data := PageData{
-			PageTitle: "Lexical Analysis - Result",
-			Tokens:    tokens,
-		}
+	var tokens []token.Token
+	var tok token.Token
 
-		tmpl.Execute(w, data)
-	})
+	lxr := lexer.New(string(src))
 
-	http.ListenAndServe(":6050", nil)
+	for tok.Type != token.EOF {
+		tok = lxr.NextToken()
+		tokens = append(tokens, tok)
+	}
+
+	tmpl, err := template.New("view.html").ParseFiles("../../ui/template/view.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	data := PageData{
+		PageTitle: "Lexical Analysis - Result",
+		Tokens:    tokens,
+	}
+
+	tmpl.Execute(w, data)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.New("home.html").ParseFiles("../../ui/template/home.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	tmpl.Execute(w, nil)
 }
